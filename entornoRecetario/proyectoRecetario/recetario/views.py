8627d3e4b4d43845010f06e5f13c88e41c11b24d
Akalucia3777 <zx19student398@colegiovalledelmiro.es>
2023-01-19 09:57:15 +0100

    hechos los templates

--- entornoRecetario/proyectoRecetario/recetario/views.py
+++ entornoRecetario/proyectoRecetario/recetario/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def inicio(request):
+    home = loader.get_template('inicio.html')
+    return HttpResponse(home.render())
+
+
+def receta(request):
+    receta = loader.get_template('receta.html')
+    return HttpResponse(receta.render())
