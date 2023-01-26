9e44ac179c6c0fc619eb3de81719d8a2dd5e1e95
Akalucia3777 <zx19student398@colegiovalledelmiro.es>
2023-01-26 09:26:42 +0100

    cambios y estilos

--- entornoRecetario/proyectoRecetario/recetario/views.py
+++ entornoRecetario/proyectoRecetario/recetario/views.py
@@ -4,26 +4,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def inicio(request):
-    home = loader.get_template('inicio.html')
+    home = loader.get_template('recetario/inicio.html')
     return HttpResponse(home.render())
 
-
 def receta(request):
-    receta = loader.get_template('receta.html')
+    receta = loader.get_template('recetario/receta.html')
     return HttpResponse(receta.render())
 
 def desayuno(request):
-    desayuno = loader.get_template('desayuno.html')
+    desayuno = loader.get_template('recetario/desayuno.html')
     return HttpResponse(desayuno.render())
 
 def comidas(request):
-    comidas = loader.get_template('comidas.html')
+    comidas = loader.get_template('recetario/comidas.html')
     return HttpResponse(comidas.render())
 
 def cenas(request):
-    cenas = loader.get_template('cenas.html')
+    cenas = loader.get_template('recetario/cenas.html')
     return HttpResponse(cenas.render())
 
 def todas(request):
-    todas = loader.get_template('todas.html')
+    todas = loader.get_template('recetario/todas.html')
     return HttpResponse(todas.render())
