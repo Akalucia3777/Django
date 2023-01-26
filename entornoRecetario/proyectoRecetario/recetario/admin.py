b390fbd4d52ce68cb295e390bf9099d82018faf3
Akalucia3777 <zx19student398@colegiovalledelmiro.es>
2023-01-26 09:10:57 +0100

    dia martes cambios aplicados en BBDD y views

--- entornoRecetario/proyectoRecetario/recetario/admin.py
+++ entornoRecetario/proyectoRecetario/recetario/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Receta,Categoria  
+from .models import Receta,Categorias  
 
 admin.site.register(Receta)
-admin.site.register(Categoria)
+admin.site.register(Categorias)
--- entornoRecetario/proyectoRecetario/recetario/models.py
+++ entornoRecetario/proyectoRecetario/recetario/models.py
@@ -1,20 +1,40 @@
+from email.policy import default
+from tabnanny import verbose
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
+# Create your models here.
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=255);
+    descripcion = models.TextField(null=True);
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fehca de creacion')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de modificacion')
+    
+    class Meta:
+        verbose_name='categoria'
+        verbose_name_plural='categoria'
+        ordering=['-created']
+        
+    def __str__(self):
+        return self.nombre
+    
 class Receta(models.Model):
-    nombre = models.CharField(max_length=255)
-    subNombre = models.CharField(max_length=255,null=True)
-    imagen = models.CharField(max_length=255,null=True)
-    ingredientes = models.TextField(null=True)
-    receta = models.TextField(null=True)
+    nombre = models.CharField(max_length=255);
+    subnombre= models.CharField(max_length=255,null=True);
+    imagen = models.ImageField(verbose_name='foto receta', upload_to='recetario')
+    ingredientes = models.TextField(null=True);
+    receta = models.TextField(null=True);
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creacion')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de modificacion')
+    author = models.ForeignKey(User, verbose_name='autor', on_delete=models.CASCADE)
+    categorias = models.ManyToManyField(Categorias,verbose_name='categorias')
     
-    #si creas un objeto de la clase receta y le haces "print" te devuelve el return
-    
-    def __str__(self) -> str:
-        return self.nombre
-
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=255)
-    descripcion = models.TextField(null=True)
-    
-    def __str__(self) -> str:
-        return self.nombre
+    class Meta:
+        verbose_name='receta'
+        verbose_name_plural='recetas'
+        ordering=['-created']
+        
+    def __str__(self):
+        return self.nombre #un to String en java 
