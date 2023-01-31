9e27fcf8e69106aedfb45f84e7495cb75ae396cc
Akalucia3777 <zx19student398@colegiovalledelmiro.es>
2023-01-31 11:55:00 +0100

    pagina admin completada

--- entornoRecetarioV2/proyectoRecetarioV2/recetario/admin.py
+++ entornoRecetarioV2/proyectoRecetarioV2/recetario/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Receta, Categorias
 
-admin.site.register(Receta)
-admin.site.register(Categorias)
+class RecetaAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+    list_display=('nombre','subnombre','author')
+    ordering=('nombre','author')
+                                        #Se pone doble _ para que busque por nombre o username
+    search_fields=('nombre','subnombre','author__username','categorias__nombre')
+    date_hierarchy=('created')
+    list_filter=('categorias__nombre','author__username')
+
+class CategoriasAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+    list_display=('nombre','descripcion')
+
+
+admin.site.register(Receta,RecetaAdmin)
+admin.site.register(Categorias,CategoriasAdmin)
 
