c2029116cc651bfb055bf6154f5425fafebb6901
Akalucia3777 <zx19student398@colegiovalledelmiro.es>
2023-02-01 13:57:59 +0100

    hasta detail y list creado

--- entornoAgendaEmpresas/agendaEmpresas/principal/urls.py
+++ entornoAgendaEmpresas/agendaEmpresas/principal/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import EmpresaListView,EmpresaDetailView
 
 urlpatterns=[
-    path('',views.listado,name='listado'),
+    path('',EmpresaListView.as_view(),name='listado'),
+    path('empresa/<int:pk>',EmpresaDetailView.as_view(),name='detalle'),
 ]
--- entornoAgendaEmpresas/agendaEmpresas/principal/views.py
+++ entornoAgendaEmpresas/agendaEmpresas/principal/views.py
@@ -2,17 +2,27 @@ from django.http import HttpResponse
 from django.shortcuts import render,get_list_or_404
 from django.template import loader
 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Empresas
 # Create your views here.
 
-def listado(request):
-    #modelo
-    empresas = get_list_or_404(Empresas)
-    #contexto
-    ctx={
-        'empresas':empresas,
-    }
-    #template
-    listadoHTML = loader.get_template('principal/listado.html')
-    #return template
-    return HttpResponse(listadoHTML.render(ctx,request))
+class EmpresaListView(ListView):
+    model = Empresas
+
+class EmpresaDetailView(DetailView):
+    model = Empresas
+    
+
+# def listado(request):
+#     #modelo
+#     empresas = get_list_or_404(Empresas)
+#     #contexto
+#     ctx={
+#         'empresas':empresas,
+#     }
+#     #template
+#     listadoHTML = loader.get_template('principal/listado.html')
+#     #return template
+#     return HttpResponse(listadoHTML.render(ctx,request))
