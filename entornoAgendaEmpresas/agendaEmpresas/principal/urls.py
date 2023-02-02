14d30e1304e6f81701cefee9ef92647363ccb607
Akalucia3777 <zx19student398@colegiovalledelmiro.es>
2023-02-02 09:34:17 +0100

    creado los py y templates de borrar y modificar

--- entornoAgendaEmpresas/agendaEmpresas/principal/urls.py
+++ entornoAgendaEmpresas/agendaEmpresas/principal/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import EmpresaListView,EmpresaDetailView,EmpresaCreateView
+from .views import EmpresaListView,EmpresaDetailView,EmpresaCreateView,EmpresaUpdateView,EmpresaDeleteView
 
 urlpatterns=[
     path('',EmpresaListView.as_view(),name='listado'),
-    path('crearEmpresa',EmpresaCreateView.as_view(),name='crear'),
     path('empresa/<int:pk>',EmpresaDetailView.as_view(),name='detalle'),
+    path('crearEmpresa',EmpresaCreateView.as_view(),name='crear'),
+    path('modificarEmpresa',EmpresaUpdateView.as_view(),name='modificar'),
+    path('borrarEmpresa/<int:pk>',EmpresaDeleteView.as_view(),name='borrar'),
     
 ]
--- entornoAgendaEmpresas/agendaEmpresas/principal/views.py
+++ entornoAgendaEmpresas/agendaEmpresas/principal/views.py
@@ -6,9 +6,9 @@ from django.template import loader
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 
-from django.urls import reverse
+from django.urls import reverse_lazy
 from .models import Empresas
 # Create your views here.
 
@@ -22,8 +22,20 @@ class EmpresaCreateView(CreateView):
     model = Empresas
     fields = ['nombre','tipo','direccion','telefono','personaContacto']
     
-    def get_success_url(self):
-        return reverse('listado')
+    #un atajo mas rapido
+    success_url = reverse_lazy('listado')
+    
+    #esto se usa mucho por los que se crea un atajo de perezosos
+    # def get_success_url(self):
+    #     return reverse('listado')
+
+class EmpresaUpdateView(UpdateView):
+    model = Empresas
+    
+class EmpresaDeleteView(DeleteView):
+    model = Empresas
+    
+    success_url = reverse_lazy('listado')
 
 # def listado(request):
 #     #modelo
